b3b883993292846eded879c0e42c33fd6d0e559e
Caue Kepler <caue.kepler@dimy.lan>
2026-02-23 22:56:37 -0300

    fix vite base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/AJM-ACOS/",
   server: {
     host: "::",
     port: 8080,
